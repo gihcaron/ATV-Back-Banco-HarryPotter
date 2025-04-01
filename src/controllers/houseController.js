5ca408e5f2630a96d9929a920155b970e53a4678
Giovanna <giovanna.c.barros@aluno.senai.br>
2025-04-01 10:25:07 -0300

    feat: create api

---
+++ src/controllers/houseController.js
@@ -0,0 +1,61 @@
+const houseModel = require("../models/houseModel");
+
+const getAllHouses = async (req, res) => {
+    try {
+        const houses = await houseModel.getHouses();
+        res.json(houses);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar casas." });
+    }
+};
+
+const getHouse = async (req, res) => {
+    try {
+        const house = await houseModel.getHouseById(req.params.id);
+        if (!house) {
+            return res.status(404).json({ message: "Casa não encontrada." });
+        }
+        res.json(house);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar casa." });
+    }
+};
+
+const createHouse = async (req, res) => {
+    try {
+        const { name, founder } = req.body;
+        const newHouse = await houseModel.createHouse(name, founder);
+        res.status(201).json(newHouse);
+    } catch (error) {
+	 console.log(error);
+        if (error.code === "23505") { 
+            return res.status(400).json({ message: "Casa já cadastrado." });
+        }
+        res.status(500).json({ message: "Erro ao criar Casa." });
+    }
+''
+};
+
+const updateHouse = async (req, res) => {
+    try {
+        const { name, founder } = req.body;
+        const updateHouse = await houseModel.updateHouse(req.params.id, name, founder);
+        if (!updateHouse) {
+            return res.status(404).json({ message: "House não encontrado." });
+        }
+        res.json(updateHouse);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao atualizar House." });
+    }
+}
+
+const deleteHouse = async (req, res) => {
+    try {
+        const message = await houseModel.deleteHouse(req.params.id);
+        res.json(message);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao deletar House." });
+    }
+};
+
+module.exports = { getAllHouses, getHouse, createHouse, updateHouse, deleteHouse};
